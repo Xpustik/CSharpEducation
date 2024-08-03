6976ed5c046721ea103ff526355a31cee894b1a3
Aleksandr <aspgloom@mail.ru>
2024-08-03 10:48:35 +0400

    added Practice2.Task7

---
+++ CifraAcademy/CSharpEducation.Practice/Practice2.Task7/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+using System.Globalization;
+
+WriteLine("Введите строку");
+
+string strValue = string.Empty;
+string str = ReadLine();
+WriteLine(str.ToUpper());
+WriteLine(str.ToLower());
+
+TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
+
+WriteLine(ti.ToTitleCase(str));
+
+/*
+string str2=str.ToLower();
+WriteLine(str2 + " " + str);
+str2 = str2.ToUpper();
+
+foreach (var item in str)
+{
+    if (char.IsLower(item))
+        strValue += char.ToUpper(item);
+   
+}
+WriteLine(str.ToLower());
+WriteLine(strValue);
+strValue = string.Empty;
+
+foreach (var item in str)
+{
+    if (char.IsUpper(item))
+        strValue += char.ToLower(item);
+
+}
+
+WriteLine(strValue);
+*/
